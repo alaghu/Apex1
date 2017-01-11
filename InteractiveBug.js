959c6367278b0beabac93623c249034e0bc7431e
Anand Alaghu <anand@loremore.com>
2017-01-11 07:15:11 -0800

    Using a single Object till Hoisting issue be understood and know how to initialize an object referencing another object (in Debug Bran

--- InteractiveBug.js
+++ InteractiveBug.js
@@ -4,26 +4,25 @@
 // This pattern is completely inspired from
 // https://www.youtube.com/playlist?list=PLoYCgNOIyGABs-wDaaxChu82q_xQgUb4f
 
-// This finally worked. We are able to obtain the list of bugs (19-Dec)
+// Using a single Object till Hoisting issue be understood and know how
+// to initialize an object referencing another object (in Debug Branch)
 
 //    TODO
-//    The problem I am facing on 08-Jan.
-//    I am unable to access interactiveReportRegions cachedom object in
-//      displayOfBugList.
-//  This should be fundamental to understanding javascript.
-//    Things to try
-
-//1. Display the typeof for $interactiveReport in   interactiveReportRegions.logging
-//2. Create a simple gist with two object and use case for question in Stack Overflow
-//3. Pass this object into the init() function of displayOfBugList
 
 // This is object literal pattern
+
+// Wanted features
+// 1. Position it right above the Bug header cell
+// 2. Display count bubble.
+// 3. On Hover display more details for clicking
+// 4. If No bugs, display zero bubble and disappear
+// 5. If bug is control broken, only one bug bubble
+
 // This object will Analyze the Interactive Report and obtain the list of bugs
 var interactiveReportRegion = {
     // List of bugs from the interactive report
     listOfBugsFromReport: [],
-    // Some Properties on analyzing the regions
-    isTheTableOfBugsPresent: false,
+
     // The initializing function - All functions should be called from here.
     init: function () {
         this.cacheDom ();
@@ -32,31 +31,35 @@ var interactiveReportRegion = {
         this.render ();
         this.logging ();
     },
+
     // Fetching the interactive report - Only once!
     cacheDom: function () {
         // Find Interactive Report's DOM element
         // The variables used to assign objects from JQuery will have
         // $ sign as prefix
         // TODO : This has to be variablized. Right now it is hard coded
-        this.$interactiveReport = $ ('#BugPreventiveIR_data_panel');
+        this.$interactiveReport             = $ ( '#BugPreventiveIR_data_panel' );
+        this.$tableIndsideInteractiveReport = this.$interactiveReport.find ( 'table' );
+        this.$tableRows                     = this.$tableIndsideInteractiveReport.find ( 'tr' );
+
+        // The Table cell (TD) , which has headers headers attribute that starts with
+        this.$rowsContainingBugs           = this.$tableRows.find ( "td[headers^='BugNumber']" );
+        this.$linkInsideRowsContainingBugs = this.$rowsContainingBugs.find ( "a" );
+        this.$bugNumberHeaderCell          = this.$tableRows.find ( "th#BugNumber" );
     },
+
     // TODO: This can be commented out later
     // Any Rendering of the UI goes here
     render: function () {
+
+        this.$bugNumberHeaderCell.css ( {"background-color": "yellow"} );
+
         // This iterates through the object and gets the html (The bug number)
-        this.$linkInsideRowsContainingBugs.each (function () {
-            $ (this).append (' - ' + $ (this).html ());
-        });
+        this.$linkInsideRowsContainingBugs.each ( function () {
+            $ ( this ).append ( ' - ' + $ ( this ).html () );
+        } );
     },
-    //  Analyze and Inspect the Interactive Report
-    analyzeInteractiveReport: function () {
-        // If the table Inside the interactive Report is present,
-        // set the isTheTableOfBugsPresent to true
-        this.$tableIndsideInteractiveReport = this.$interactiveReport.find ('table');
-        if (this.$tableIndsideInteractiveReport.length !== 0) {
-            this.isTheTableOfBugsPresent = true;
-        }
-    },
+
     // Get the List of Bugs
     getListofBugs: function () {
         if (this.isTheTableOfBugsPresent === true) {
@@ -64,80 +67,32 @@ var interactiveReportRegion = {
             // This can be accessed inside the each function
             var _this = this;
 
-            this.$tableRows = this.$tableIndsideInteractiveReport.find ('tr');
-
-            // The Table cell (TD) , which has headers headers attribute that starts with
-            this.$rowsContainingBugs = this.$tableRows.find ("td[headers^='BugNumber']");
-            this.$linkInsideRowsContainingBugs = this.$rowsContainingBugs.find ("a");
-
             // Iterate through this.$this.$linkInsideRowsContainingBugs
-            this.$linkInsideRowsContainingBugs.each (function (index) {
-                console.log (index + ":" + $ (this).html ());
+            this.$linkInsideRowsContainingBugs.each ( function ( index ) {
+                console.log ( index + ":" + $ ( this ).html () );
 
                 // Pushing into array
-                _this.listOfBugsFromReport.push ($ (this).html ());
+                _this.listOfBugsFromReport.push ( $ ( this ).html () );
 
-            });
+            } );
 
             this.$bugNumberInLinkInsideRowsContainingBugs = this.$linkInsideRowsContainingBugs.html ();
         }
     },
+
     // All Logging goes in here
     logging: function () {
-        console.log ("The list of bugs " + this.listOfBugsFromReport);
-        console.log ("What is  " + this.isTheInteractiveReportPresent);
-        console.log ("Is the Table of Bugs Present ? " + this.isTheTableOfBugsPresent);
-        console.log ("The table inside interactive Report " + this.$tableIndsideInteractiveReport);
-        console.log ("The Number of Table Rows = " + this.$tableRows.length);
+        console.log ( "The list of bugs " + this.listOfBugsFromReport );
+        console.log ( "What is  " + this.isTheInteractiveReportPresent );
+        console.log ( "Is the Table of Bugs Present ? " + this.isTheTableOfBugsPresent );
+        console.log ( "The table inside interactive Report " + this.$tableIndsideInteractiveReport );
+        console.log ( "The Number of Table Rows = " + this.$tableRows.length );
 
     }
 };
 
-//This Object will be used to derive the final BugDBLink
-var bugDBLinkForListofBugsFromReport = {
-    //Try and annotate the different parameters
-    bugDBLinkFoundation: "",
-    init: function () {
-        this.logging ();
-    },
-    // All Logging goes in here
-    logging: function () {
-        console.log (" I am inside bugDBLinkForListofBugsFromReport");
-    }
-};
-
-// This Object will deal with displaying the link
-var displayOfBugList = {
-    // Wanted features
-    // 1. Position it right above the Bug header cell
-    // 2. Display count bubble.
-    // 3. On Hover display more details for clicking
-    // 4. If No bugs, display zero bubble and disappear
-    // 5. If bug is control broken, only one bug bubble
-    $IR: interactiveReportRegion.$interactiveReport,
-    cssForDisplayRegion: {},
-    init: function () {
-        this.logging ();
-        this.render ();
-
-    },
-    // All Rendering goes in here
-    render: function () {
-        // Need to derive the table header from $interactiveReport
-
-    },
-    // All Logging goes in here
-    logging: function () {
-        console.log (" I am inside displayOfBugList");
-        console.log (this.$IR);
-    }
-};
-
-$ (document).ready (function () {
+$ ( document ).ready ( function () {
     console.clear ();
     // Invoking the Objects
     interactiveReportRegion.init ();
-    bugDBLinkForListofBugsFromReport.init ();
-    displayOfBugList.init ();
-
-});
+} );
