4a49da57117f4fd7e7efe5c31e7a4bb0e8d477f8
Anand Alaghu <anand@loremore.com>
2017-01-14 21:42:59 -0800

    some more clean up

--- bulkBugFromInteractiveReport.js
+++ bulkBugFromInteractiveReport.js
@@ -7,7 +7,6 @@
 // 2. interactiveReportRegion.init () is invoked inside document.ready
 // 3. Readme. Important!
 
-
 var interactiveReportRegion = {
     // List of bugs from the interactive report
     listOfBugsFromReport: [],
@@ -34,21 +33,22 @@ var interactiveReportRegion = {
         // The variables used to assign objects from JQuery will have
         // $ sign as prefix
         // TODO : This has to be variablized. Right now it is hard coded
-        this.$interactiveReport             = $ ('#BugPreventiveIR_data_panel');
-        this.$tableIndsideInteractiveReport = this.$interactiveReport.find ('table');
-        this.$tableRows                     = this.$tableIndsideInteractiveReport.find ('tr');
+        this.$interactiveReport = $ ( '#BugPreventiveIR_data_panel' );
+        this.$tableIndsideInteractiveReport = this.$interactiveReport.find ( 'table' );
+        this.$tableRows = this.$tableIndsideInteractiveReport.find ( 'tr' );
+        this.$bugNumberHeaderCell = this.$tableRows.find ( "th#BugNumber" );
 
         // The Table cell (TD) , which has headers headers attribute that starts with
-        this.$rowsContainingBugs           = this.$tableRows.find ("td[headers^='BugNumber']");
-        this.$linkInsideRowsContainingBugs = this.$rowsContainingBugs.find ("a");
-        this.$bugNumberHeaderCell          = this.$tableRows.find ("th#BugNumber");
+        this.$rowsContainingBugs = this.$tableRows.find ( "td[headers^='BugNumber']" );
+        this.$linkInsideRowsContainingBugs = this.$rowsContainingBugs.find ( "a" );
+
     },
 
     // TODO: This can be commented out later
     // Any Rendering of the UI goes here
     render: function () {
         // This is the overlaying DIV
-        this.$bugNumberHeaderCell.append (this.$htmlForLink);
+        this.$bugNumberHeaderCell.append ( this.$htmlForLink );
     },
 
     // Get the List of Bugs
@@ -58,13 +58,13 @@ var interactiveReportRegion = {
         var _this = this;
 
         // Iterate through this.$this.$linkInsideRowsContainingBugs
-        this.$linkInsideRowsContainingBugs.each (function (index) {
-            console.log (index + ":" + $ (this).html ());
+        this.$linkInsideRowsContainingBugs.each ( function ( index ) {
+            console.log ( index + ":" + $ ( this ).html () );
 
             // Pushing into array
-            _this.listOfBugsFromReport.push ($ (this).html ());
+            _this.listOfBugsFromReport.push ( $ ( this ).html () );
 
-        });
+        } );
 
         this.$bugNumberInLinkInsideRowsContainingBugs = this.$linkInsideRowsContainingBugs.html ();
 
@@ -74,48 +74,47 @@ var interactiveReportRegion = {
     createFinalBugLink: function () {
         //itereate through listOfBugsFromReport
         this.listOfBugsAsConcatentatedText = "";
-        this.listOfBugsAsConcatentatedText = this.listOfBugsFromReport.join (",");
-        this.finalBugDBLink                = this.bugDBBaseURL + this.listOfBugsAsConcatentatedText +
+        this.listOfBugsAsConcatentatedText = this.listOfBugsFromReport.join ( "," );
+        this.finalBugDBLink = this.bugDBBaseURL + this.listOfBugsAsConcatentatedText +
             "&fid_arr=40&fcont_arr=2&fid_arr=100&cid_arr=2&cid_arr=27&cid_arr=9&cid_arr=8&cid_arr=7&cid_arr=11&cid_arr=6&cid_arr=12&cid_arr=13&f_count=3&c_count=9&query_type=1";
 
     },
     finalHTML         : function () {
 
-        this.$htmlForLink = $ ('<a></a>').attr ("href", this.finalBugDBLink).html ('<span class="badge bg-green">' + this.listOfBugsFromReport.length + '</span>');
+        this.$htmlForLink = $ ( '<a></a>' ).attr ( "href", this.finalBugDBLink ).html ( '<span class="badge bg-green">' + this.listOfBugsFromReport.length + '</span>' );
 
     },
 
     // testing events
     bindEvents: function () {
-        this.$tableRows.on('click',this.alertTest.bind(this));
+        this.$tableRows.on ( 'click', this.alertTest.bind ( this ) );
 
     },
-    alertTest: function () {
-        alert("hi there");
+    alertTest : function () {
+        alert ( "hi there" );
     },
     // All Logging goes in here
-    logging           : function () {
-        console.log ("The list of bugs " + this.listOfBugsFromReport);
-        console.log ("The Number of Bugs  = " + this.listOfBugsFromReport.length);
-        console.log ("The concatenated list of bug " + this.listOfBugsAsConcatentatedText);
-        console.log (" html for link " + this.$htmlForLink);
+    logging   : function () {
+        console.log ( "The list of bugs " + this.listOfBugsFromReport );
+        console.log ( "The Number of Bugs  = " + this.listOfBugsFromReport.length );
+        console.log ( "The concatenated list of bug " + this.listOfBugsAsConcatentatedText );
+        console.log ( " html for link " + this.$htmlForLink );
 
     }
 };
 
-$ (document).ready (function () {
+$ ( document ).ready ( function () {
     console.clear ();
     // Invoking the Objects
     interactiveReportRegion.init ();
-});
+} );
 
 //Refer to this for understanding refresh
 //http://vmorneau.me/using-apex-javascript-events/
-$(window).on("click", function(e) {
-    console.log(e.type);
-    console.log("Anand , the page is refreshed");
-});
-
+$ ( window ).on ( "click", function ( e ) {
+    console.log ( e.type );
+    console.log ( "Anand , the page is refreshed" );
+} );
 
 // ------------Readme---------------------
 // Questions
@@ -159,15 +158,9 @@ $(window).on("click", function(e) {
 //What are the dependencies?
 // We need to innclude badges.css.
 
-
-
-
-
 //--------------Other Notes-----------------
 // This pattern, object literal, is completely inspired from
 // https://www.youtube.com/playlist?list=PLoYCgNOIyGABs-wDaaxChu82q_xQgUb4f
-
-
 
 //    TODO
 // Wanted features
