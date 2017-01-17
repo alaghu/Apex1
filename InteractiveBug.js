3a0e9cb32ec261ae7e257707174aa4299fc08e66
anand@loremore.com <anand@loremore.com>
2017-01-17 09:33:17 -0800

    Trying out events.

--- InteractiveBug.js
+++ InteractiveBug.js
@@ -4,6 +4,7 @@
 // This pattern is completely inspired from
 // https://www.youtube.com/playlist?list=PLoYCgNOIyGABs-wDaaxChu82q_xQgUb4f
 
+<<<<<<< .merge_file_a05476
 // Using a single Object till Hoisting issue be understood and know how
 // to initialize an object referencing another object (in Debug Branch)
 
@@ -36,6 +37,17 @@ var interactiveReportRegion = {
         this.createFinalBugLink ();
         this.finalHTML ();
         this.render ();
+=======
+// This is object literal pattern
+// This object will Analyze the Interactive Report and obtain the list of bugs
+
+// This Object will deal with displaying the link
+
+
+var interactiveReportRegion = {
+    init: function () {
+        this.cacheDom ();
+>>>>>>> .merge_file_a07600
         this.logging ();
     },
 
@@ -44,6 +56,7 @@ var interactiveReportRegion = {
         // Find Interactive Report's DOM element
         // The variables used to assign objects from JQuery will have
         // $ sign as prefix
+<<<<<<< .merge_file_a05476
         // TODO : This has to be variablized. Right now it is hard coded
         this.$interactiveReport             = $ ('#BugPreventiveIR_data_panel');
         this.$tableIndsideInteractiveReport = this.$interactiveReport.find ('table');
@@ -110,15 +123,48 @@ var interactiveReportRegion = {
         console.log ("The Number of Bugs  = " + this.listOfBugsFromReport.length);
         console.log ("The concatenated list of bug " + this.listOfBugsAsConcatentatedText);
         console.log (" html for link " + this.$htmlForLink);
+=======
+        this.$interactiveReport = $ ('#BugPreventiveIR_data_panel');
+    },
+    // All Logging goes in here
+    logging: function () {
+        console.log ("The interactive Report " + this.$interactiveReport);
+    }
+};
+
+var displayOfBugList = {
+    $IR: interactiveReportRegion.$interactiveReport,
+    a: "Thanks Rahul",
+    // Assiging to a
+    init: function () {
+        //this.logging ();
+        // this.$IR = interactiveReportRegion.$interactiveReport;
+    },
+    // All Logging goes in here
+    logging: function () {
+        console.log (" I am inside displayOfBugList");
+        // Similairly even this comes up as undedefined but is accessible through console
+        console.log (" This is $interactive report of OBJ1 " + interactiveReportRegion.$interactiveReport);
+        // This comes up as undefined as well as in console
+        console.log (" This is $IR of OBJ2 " + this.$IR);
+        console.log (" This is a " + this.a);
+>>>>>>> .merge_file_a07600
 
     }
 };
 
+// Invoking the respective init functions onlu
 $ (document).ready (function () {
     console.clear ();
-    // Invoking the Objects
+    // Invoking the Objects only after document is ready.
     interactiveReportRegion.init ();
+<<<<<<< .merge_file_a05476
 });
+=======
+
+    // displayOfBugList.init();
+    displayOfBugList.logging ();
+>>>>>>> .merge_file_a07600
 
 //Refer to this for understanding refresh
 //http://vmorneau.me/using-apex-javascript-events/
